deafc058a9004d03f281cdcc7c84f02268f9f57a
Ingo Bauersachs <ingo@jitsi.org>
2013-01-29 00:52:54 +0000

    1.0.1

--- src/ch/imvs/sdes4j/CryptoAttribute.java
+++ src/ch/imvs/sdes4j/CryptoAttribute.java
@@ -24,7 +24,7 @@ public class CryptoAttribute {
     protected int tag;
     protected CryptoSuite cryptoSuite;
     protected KeyParam[] keyParams;
-    protected SessionParam[] sessionParams = null;
+    protected SessionParam[] sessionParams;
 
     protected CryptoAttribute(){
     }
@@ -47,49 +47,10 @@ public class CryptoAttribute {
                 tokens.add(s);
         }
 
-        result.setTag(tokens.remove(0));
-        result.setCryptoSuite(tokens.remove(0), f);
-
-        if (tokens.size() < 1)
-            throw new IllegalArgumentException("There must be at least one key parameter");
-        result.setKeyParams(tokens.remove(0), f);
-
+        result.setTag(tokens);
+        result.setCryptoSuite(tokens, f);
+        result.setKeyParams(tokens, f);
         result.setSessionParams(tokens, f);
-        return result;
-    }
-
-    /**
-     * Creates an instance of a CryptoAttribute from a SDes attributes (tag,
-     * crypto suite, key params and session params).
-     *
-     * @param tag unparsed tag as a string. 
-     * @param cryptoSuite the crypto suite as an unparsed string.
-     * @param keyParams An unparsed string representation of the key param list
-     * (each key must be separated by a ";").
-     * @param sessionParams An unparsed string representation of the session
-     * param list (each key must be separated by a " ").
-     * @param f factory that creates the instances for each part of the
-     *            attribute
-     *
-     * @return a parsed crypto attribute
-     */
-    public static CryptoAttribute create(String tag, String cryptoSuite, String keyParams, String sessionParams, SDesFactory f) {
-        CryptoAttribute result = f.createCryptoAttribute();
-
-        result.setTag(tag);
-        result.setCryptoSuite(cryptoSuite, f);
-        result.setKeyParams(keyParams, f);
-
-        List<String> tokens = new LinkedList<String>();
-        if(sessionParams != null) {
-            for (String s : sessionParams.split("\\s")) {
-                if(s.trim().length() > 0)
-                    tokens.add(s);
-            }
-        }
-
-        result.setSessionParams(tokens, f);
-
         return result;
     }
 
@@ -103,23 +64,10 @@ public class CryptoAttribute {
      * @param sessionParams the additional key parameters
      */
     public CryptoAttribute(int tag, CryptoSuite cryptoSuite, KeyParam[] keyParams, SessionParam[] sessionParams) {
-        if (tag > 99999999 || tag < 0)
-            throw new IllegalArgumentException("tag can have at most 10 digits and must be non-negative");
-
-        if (cryptoSuite == null)
-            throw new IllegalArgumentException("cryptoSuite cannot be null");
-
-        if (keyParams == null || keyParams.length == 0)
-            throw new IllegalArgumentException("keyParams cannot be null or empty");
-
         this.tag = tag;
         this.cryptoSuite = cryptoSuite;
         this.keyParams = keyParams;
-
-        if (sessionParams == null)
-            this.sessionParams = new SessionParam[0];
-        else
-            this.sessionParams = sessionParams;
+        this.sessionParams = sessionParams;
     }
 
     /**
@@ -136,10 +84,12 @@ public class CryptoAttribute {
      * attribute. The tag MUST be unique among all crypto attributes for a given
      * media line.
      * 
-     * @param stringTag unparsed tag as a string. 
+     * @param tokens List of String-Tokens where the matching items are consumed
+     *            and removed from the list
+     * @param f
      */
-    private void setTag(String stringTag) {
-        int tag = Integer.valueOf(stringTag);
+    private void setTag(List<String> tokens) {
+        int tag = Integer.valueOf(tokens.remove(0));
         if (tag > 99999999 || tag < 0)
             throw new IllegalArgumentException("tag can have at most 10 digits and must be non-negative");
         this.tag = tag;
@@ -159,11 +109,12 @@ public class CryptoAttribute {
      * Sets the identifier that describes the encryption and authentication
      * algorithms (e.g., AES_CM_128_HMAC_SHA1_80) for the transport in question.
      * 
-     * @param stringCryptoSuite the crypto suite as an unparsed string.
+     * @param tokens List of String-Tokens where the matching items are consumed
+     *            and removed from the list
      * @param f factory that creates the crypto suite instance
      */
-    private void setCryptoSuite(String stringCryptoSuite, SDesFactory f) {
-        this.cryptoSuite = f.createCryptoSuite(stringCryptoSuite);
+    private void setCryptoSuite(List<String> tokens, SDesFactory f) {
+        this.cryptoSuite = f.createCryptoSuite(tokens.remove(0));
     }
 
     /**
@@ -180,12 +131,15 @@ public class CryptoAttribute {
      * Sets one or more sets of keying material for the crypto-suite in
      * question.
      * 
-     * @param stringKeyParams An unparsed string representation of the key param
-     * list (each key must be separated by a ";").
+     * @param tokens List of String-Tokens where the matching items are consumed
+     *            and removed from the list
      * @param f factory that creates the key params instances
      */
-    private void setKeyParams(String stringKeyParams, SDesFactory f) {
-        String[] params = stringKeyParams.split(";");
+    private void setKeyParams(List<String> tokens, SDesFactory f) {
+        if (tokens.size() < 1)
+            throw new IllegalArgumentException("There must be at least one key parameter");
+
+        String[] params = tokens.remove(0).split(";");
         List<KeyParam> keyParams = new LinkedList<KeyParam>();
         for (String p : params) {
             keyParams.add(f.createKeyParam(p));
@@ -229,52 +183,17 @@ public class CryptoAttribute {
         sb.append(' ');
         sb.append(cryptoSuite.encode());
         sb.append(' ');
-        sb.append(getKeyParamsString());
-        if (sessionParams != null && sessionParams.length > 0) {
-            sb.append(' ');
-            sb.append(getSessionParamsString());
-        }
-
-        return sb.toString();
-    }
-
-    /**
-     * Returns a string representation the key parameters according to the ABNF
-     * rule key-params.
-     * 
-     * @return String representation of the list of key params separated by ";".
-     */
-    public String getKeyParamsString() {
-        StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < keyParams.length; i++) {
             sb.append(keyParams[i].encode());
             if (i < keyParams.length - 1)
                 sb.append(';');
         }
-
-        return sb.toString();
-    }
-
-    /**
-     * Returns a string representation of the session parameters according to
-     * the ABNF rule session-param.
-     *
-     * @return Returns a string representation of the list of session params
-     * separated by " ", or null if there are no session params.
-     */
-    public String getSessionParamsString() {
-        if (sessionParams != null && sessionParams.length > 0) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < sessionParams.length; i++) {
-                sb.append(sessionParams[i].encode());
-                if (i < sessionParams.length - 1)
-                    sb.append(' ');
+        if (sessionParams != null) {
+            for (SessionParam p : sessionParams) {
+                sb.append(' ');
+                sb.append(p.encode());
             }
-            return sb.toString();
         }
-        else{
-            return null;
-        }
+        return sb.toString();
     }
 }
--- test/test/ch/imvs/sdes4j/CryptoAttributeTest.java
+++ test/test/ch/imvs/sdes4j/CryptoAttributeTest.java
@@ -21,14 +21,13 @@ import org.junit.Test;
 
 import ch.imvs.sdes4j.CryptoAttribute;
 import ch.imvs.sdes4j.KeyParam;
-import ch.imvs.sdes4j.SessionParam;
 import ch.imvs.sdes4j.srtp.FecOrderSessionParam;
-import ch.imvs.sdes4j.srtp.NoAuthSessionParam;
 import ch.imvs.sdes4j.srtp.SrtpCryptoSuite;
 import ch.imvs.sdes4j.srtp.SrtpKeyParam;
 import ch.imvs.sdes4j.srtp.SrtpSDesFactory;
 
 public class CryptoAttributeTest {
+
     private SrtpSDesFactory f;
 
     @Before
@@ -37,121 +36,8 @@ public class CryptoAttributeTest {
     }
 
     @Test
-    public void testEncodeSessionParamsString() {
-        byte[] bkey = new byte[]{0};
-        SrtpCryptoSuite suite = f.createCryptoSuite(SrtpCryptoSuite.AES_CM_128_HMAC_SHA1_80);
-        SrtpKeyParam key = new SrtpKeyParam(
-                SrtpKeyParam.KEYMETHOD_INLINE,
-                bkey,
-                0, 0, 0
-        );
-        CryptoAttribute a = new CryptoAttribute(
-                1,
-                suite,
-                new KeyParam[] { key },
-                new SessionParam[] {
-                        new FecOrderSessionParam(FecOrderSessionParam.FEC_SRTP),
-                        new NoAuthSessionParam()
-                }
-        );
-        assertEquals(2, a.getSessionParams().length);
-        assertEquals("FEC_ORDER=FEC_SRTP UNAUTHENTICATED_SRTP", a.getSessionParamsString());
-    }
-
-    @Test
-    public void testEncodeKeyParamsString() {
-        byte[] bkey = new byte[]{0};
-        SrtpCryptoSuite suite = f.createCryptoSuite(SrtpCryptoSuite.AES_CM_128_HMAC_SHA1_80);
-        SrtpKeyParam key = new SrtpKeyParam(
-                SrtpKeyParam.KEYMETHOD_INLINE,
-                bkey,
-                0, 0, 0
-        );
-        CryptoAttribute a = new CryptoAttribute(
-                1,
-                suite,
-                new KeyParam[] { key, key },
-                null
-        );
-        assertEquals(2, a.getKeyParams().length);
-        assertEquals("inline:AA==;inline:AA==", a.getKeyParamsString());
-        assertEquals(0, a.getSessionParams().length);
-    }
-
-    @Test
-    public void testParseMultipleKeyParams() {
-        CryptoAttribute a = CryptoAttribute.create("1 AES_CM_128_HMAC_SHA1_80 inline:AA==|2^20|1:4;inline:AA==;inline:AA==|1024", f);
-        assertEquals(3, a.getKeyParams().length);
-
-        SrtpKeyParam kp1 = (SrtpKeyParam) a.getKeyParams()[0];
-        assertEquals(SrtpKeyParam.KEYMETHOD_INLINE, kp1.getKeyMethod());
-        assertEquals(0x100000, kp1.getLifetime());
-        assertEquals(1, kp1.getMki());
-        assertEquals(4, kp1.getMkiLength());
-
-        SrtpKeyParam kp2 = (SrtpKeyParam) a.getKeyParams()[1];
-        assertEquals(SrtpKeyParam.KEYMETHOD_INLINE, kp2.getKeyMethod());
-        assertEquals(0, kp2.getLifetime());
-        assertEquals(0, kp2.getMki());
-        assertEquals(0, kp2.getMkiLength());
-
-        SrtpKeyParam kp3 = (SrtpKeyParam) a.getKeyParams()[2];
-        assertEquals(SrtpKeyParam.KEYMETHOD_INLINE, kp3.getKeyMethod());
-        assertEquals(1024, kp3.getLifetime());
-        assertEquals(0, kp3.getMki());
-        assertEquals(0, kp3.getMkiLength());
-
-        assertEquals("inline:AA==|1048576|1:4;inline:AA==;inline:AA==|1024", a.getKeyParamsString());
-    }
-
-    @Test
     public void testParseExample1() {
         CryptoAttribute a = CryptoAttribute.create("1 AES_CM_128_HMAC_SHA1_80       inline:WVNfX19zZW1jdGwgKCkgewkyMjA7fQp9CnVubGVz|2^20|1:4 FEC_ORDER=FEC_SRTP", f);
-        validateExample2(a);
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testParseWithoutKeyParameter() {
-        new CryptoAttribute(1, new SrtpCryptoSuite(SrtpCryptoSuite.AES_CM_128_HMAC_SHA1_80), null, null);
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testParseWithoutKeyParameterString() {
-        CryptoAttribute.create("1 AES_CM_128_HMAC_SHA1_80", f);
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testParseWithTagAboveLimitString() {
-        CryptoAttribute.create("1234567890 AES_CM_128_HMAC_SHA1_80", f);
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testParseWithTagBelowLimitString() {
-        CryptoAttribute.create("-1 AES_CM_128_HMAC_SHA1_80", f);
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testParseWithTagAboveLimit() {
-        new CryptoAttribute(1234567890, null, null, null);
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testParseWithTagBelowLimit() {
-        new CryptoAttribute(-1, null, null, null);
-    }
-
-    @Test(expected=IllegalArgumentException.class)
-    public void testParseWithoutCryptoSuite() {
-        new CryptoAttribute(1, null, null, null);
-    }
-
-    @Test
-    public void testParseExample1Xmpp() {
-        CryptoAttribute a = CryptoAttribute.create("1", "AES_CM_128_HMAC_SHA1_80", "inline:WVNfX19zZW1jdGwgKCkgewkyMjA7fQp9CnVubGVz|2^20|1:4", "FEC_ORDER=FEC_SRTP", f);
-        validateExample2(a);
-    }
-
-    private void validateExample2(CryptoAttribute a) {
         assertEquals(1, a.getTag());
         assertEquals(SrtpCryptoSuite.AES_CM_128_HMAC_SHA1_80, a.getCryptoSuite().encode());
         assertEquals(1, a.getKeyParams().length);
@@ -162,7 +48,6 @@ public class CryptoAttributeTest {
         assertEquals(4, kp.getMkiLength());
         assertEquals(1, a.getSessionParams().length);
         assertEquals(FecOrderSessionParam.FEC_SRTP, ((FecOrderSessionParam) a.getSessionParams()[0]).getMode());
-        assertEquals("FEC_ORDER=FEC_SRTP", a.getSessionParamsString());
     }
 
     @Test
@@ -180,28 +65,11 @@ public class CryptoAttributeTest {
         assertEquals(2, ((SrtpKeyParam) a.getKeyParams()[1]).getMki());
         assertEquals(1, a.getSessionParams().length);
         assertEquals(FecOrderSessionParam.FEC_SRTP, ((FecOrderSessionParam) a.getSessionParams()[0]).getMode());
-        assertEquals("2 F8_128_HMAC_SHA1_80 inline:MTIzNDU2Nzg5QUJDREUwMTIzNDU2Nzg5QUJjZGVm|1048576|1:4;inline:QUJjZGVmMTIzNDU2Nzg5QUJDREUwMTIzNDU2Nzg5|1048576|2:4 FEC_ORDER=FEC_SRTP", a.encode());
     }
 
     @Test
     public void testParseExample3() {
         CryptoAttribute a = CryptoAttribute.create("1 AES_CM_128_HMAC_SHA1_80    inline:PS1uQCVeeCFCanVmcjkpPywjNWhcYD0mXXtxaVBR|2^20|1:4", f);
-        validateExample3(a);
-    }
-
-    @Test
-    public void testParseExample3Xmpp() {
-        CryptoAttribute a = CryptoAttribute.create("1", "AES_CM_128_HMAC_SHA1_80", "inline:PS1uQCVeeCFCanVmcjkpPywjNWhcYD0mXXtxaVBR|2^20|1:4", null, f);
-        validateExample3(a);
-    }
-
-    @Test
-    public void testParseExample3bXmpp() {
-        CryptoAttribute a = CryptoAttribute.create("1", "AES_CM_128_HMAC_SHA1_80", "inline:PS1uQCVeeCFCanVmcjkpPywjNWhcYD0mXXtxaVBR|2^20|1:4", "", f);
-        validateExample3(a);
-    }
-    
-    private void validateExample3(CryptoAttribute a) {
         assertEquals(1, a.getTag());
         assertEquals(SrtpCryptoSuite.AES_CM_128_HMAC_SHA1_80, a.getCryptoSuite().encode());
         assertEquals(1, a.getKeyParams().length);
@@ -211,8 +79,6 @@ public class CryptoAttributeTest {
         assertEquals(1, kp.getMki());
         assertEquals(4, kp.getMkiLength());
         assertEquals(0, a.getSessionParams().length);
-        assertEquals("inline:PS1uQCVeeCFCanVmcjkpPywjNWhcYD0mXXtxaVBR|1048576|1:4", a.getKeyParamsString());
-        assertEquals(null, a.getSessionParamsString());
     }
 
     @Test
@@ -226,6 +92,5 @@ public class CryptoAttributeTest {
         );
         CryptoAttribute a = new CryptoAttribute(1, suite, new KeyParam[] { key }, null);
         assertEquals("1 AES_CM_128_HMAC_SHA1_80 inline:AA==", a.encode());
-        assertEquals(0, a.getSessionParams().length);
     }
 }
